f5726957ac33dcc6872701221a98c98bcbfbbf3d
johan ahlqvist <johan_ahlkvist@hotmail.com>
2019-08-16 01:33:51 +0200

    fix regex not always returning

--- bot/utils.py
+++ bot/utils.py
@@ -11,7 +11,7 @@ def get_config(module_name):
 def filter_mod(m, regex):
     value = 1
     filt_mod = regex.sub('', str(m)).lower()
-    potential_value = re.findall('(^\-)?[0-9]*\.?[0-9]', m)[0]
+    potential_value = re.findall('(^\-)?[0-9]*\.?[0-9]|$', m)[0]
     if len(potential_value) > 0:
         value = float(potential_value)
     return filt_mod, value
